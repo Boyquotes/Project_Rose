64b54e15802e96e28c622a84a83589cf4e84966a
Tyler Roman <31106886+Syndicist@users.noreply.github.com>
2021-11-07 15:31:01 -0500

    more lighting stuff

--- Scripts/Actors/Player/wind.gd
+++ Scripts/Actors/Player/wind.gd
@@ -4,17 +4,25 @@ enum WindType {NONE, GENTLE, MILD, STRONG, TORRENT}
 
 export (Vector2) var wind := Vector2(0.0, 0.0)
 export (WindType) var wind_type = WindType.GENTLE
+export (bool) var directional := false
+export(float) var direction := 0.0
 var prev_type = WindType.GENTLE
 var rng = RandomNumberGenerator.new()
 var roll_wind = false
 var wind_chaos := 0.0
 
+
+
 func _ready():
 	$Timer.start(rng.randf_range(0.5, 5.0))
 	set_wind()
 
+var deltatime = 0.0
+
 func _process(_delta):
 	rng.randomize()
+	
+	deltatime += _delta
 	
 	match(wind_type):
 		WindType.NONE:
@@ -39,10 +47,13 @@ func _process(_delta):
 			wind = Vector2(2000, 0).rotated(scale.x * rotation)
 		WindType.TORRENT:
 			wind_chaos = 3
-			if roll_wind:
-				rotation += rng.randf_range(-PI/2, PI/2)
-				roll_wind = false
-			wind = Vector2(4000, 0).rotated(scale.x * rotation)
+			if directional:
+				wind = (Vector2(1000, 0) * (1.25 * (sin(deltatime + rng.randf_range(0, 4))) + .5)).rotated(direction + rng.randf_range(-PI/4, PI/4))
+			else:
+				if roll_wind:
+					rotation += rng.randf_range(-PI/2, PI/2)
+					roll_wind = false
+				wind = Vector2(4000, 0).rotated(scale.x * rotation)
 
 func set_wind():
 	match(wind_type):
